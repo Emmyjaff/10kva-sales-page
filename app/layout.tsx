12e3b39fa5212aeec17311b49a8cb70f165c4f03
Emmanuel Jafaru <jafaruemmanuel48@gmail.com>
2025-09-16 18:27:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'kairosenergy 10kva sales',
+  description: 'kairosenergy powering clean energy',
 }
 
 export default function RootLayout({
